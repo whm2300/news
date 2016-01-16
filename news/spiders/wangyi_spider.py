043426d11a832fd0d0d40fd328cdd836ae104067
michael <michael@localhost.localdomain>
2016-01-16 17:13:43 +0800

    完成提取主页标题

--- news/spiders/wangyi_spider.py
+++ news/spiders/wangyi_spider.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import logging
+import re
 
 import scrapy
 from scrapy.http.request import Request
@@ -11,6 +12,8 @@ class WangyiSpider(scrapy.Spider):
     name = "wangyi"
     allowed_domains = ["163.com"]
     start_urls = ["http://news.163.com"]
+    crawl_url = set()  #已经爬取的url
+    url_patter = re.compile(r'/\d+/\d+/\d+/*')
 
     def make_requests_from_url(self, url):
         #模拟浏览器请求
@@ -25,29 +28,43 @@ class WangyiSpider(scrapy.Spider):
 
     def parse(self, response):
         if response.status != 200:
-            self.log("get page fail:" + response.url, log.ERROR)
+            self.log("get page fail:" + response.url, logging.ERROR)
             return 
 
-        f = open('a.html', 'wb')
-        f.write(response.body)
-        return 
-
-        #links = response.xpath('///div[@class="ns-bg-wrap"]/div[@class="ns-area cf"]/div[@class="ns-main"]/div[@class="ns-mr60"]/div[@class="ns-wnews mb20"]/h3')
-        #for index, link in enumerate(links):
-        #    head_lines = link.xpath('a')
-        #    for head_line in head_lines:
-        #        #head_line.xpath('text()').extract_first()  #主页标题
-        #        yield Request(head_line.xpath('@href').extract_first(), self.parse_head_line_page)
+        linka_s = 0
+        linka_e = 0
+        while linka_s != -1:
+            linka_s = response.body.find('<a', linka_e)
+            linka_e = response.body.find('</a>', linka_s)
+            url_a = response.body[linka_s:linka_e+4]
+            url_s = url_a.find('http://')
+            url_e = url_a.find('"', url_s)
+            match = self.url_patter.search(url_a[url_s:url_e])
+            if match:
+                self.crawl_url.add(url_a[url_s:url_e])
+                yield Request(url_a[url_s:url_e], self.parse_news_page)
     
-    def parse_head_line_page(self, response):
+    def parse_news_page(self, response):
+        """解析新闻正文"""
         if response.status != 200:
-            self.log("get head line fail:" + response.url, log.ERROR)
+            self.log("get head line fail:" + response.url, logging.ERROR)
             return 
 
-        title = response.xpath('////h1/text()').extract_first()
-        content = response.xpath('////div[@id="endText"]').extract_first()
+        #过滤掉不合适网页
+        if response.url.find('http://view') != -1:
+            return 
+        if response.url.find('http://caozhi') != -1:
+            return 
+        if response.url.find('http://renjia') != -1:
+            return 
+
+        s_str = '<h1 id="h1title" class="ep-h1">'
+        e_str = '</h1>'
+        s = response.body.find(s_str)
+        e = response.body.find(e_str, s)
+
         data = NewsItem()
-        data['title'] = title
+        data['title'] = response.body[s+len(s_str):e].decode('gb2312').encode('utf-8')
         data['url'] = response.url
-        data['content'] = content
+        data['content'] = 'aaaaa'
         return data
